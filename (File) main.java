72543719e092b59dd0f66f22a6c459679d8236fe
KJanos <kjanos96@gmail.com>
2014-04-28 09:55:03 +0300

    Update (File) main.java

--- (File) main.java
+++ (File) main.java
@@ -7,8 +7,8 @@ public class Main {
         File f = new File("D:/Task text.txt");
         Scanner sc=new Scanner(System.in);
         sc=new Scanner(f);
-        String s[]=new String[10000] ;
-        int i=0; int k=0;
+        String s[]=new String[1000000] ;
+        int i=0; int k=0; int d=0;
         String s1; String s2="";
         for(i=0; sc.hasNext(); i++ ){
             s[i]=sc.nextLine();
@@ -17,13 +17,24 @@ public class Main {
             }
             s2=s[i]+s2;
         }
-        s1 = s2.replace("  ","");
+        for (int j=1; j<s2.length(); j++){
+            char c=s2.charAt(j);
+            if (c==' ') {
+                d++;
+            }
+        }
+        if (d>0) {d--;
+        }
+        String [] words = s2.split(" ");
+        d=d-(words.length);
+        /*s1 = s2.replace("  "," ");
         s1 = s2.replace("   "," ");
-        s1 = s2.replace("    "," ");
-        int l=s1.length();
-        String [] words = s1.split(" ");
+        s1 = s2.replace("    "," ");*/
+        int l=s2.length()-d;
+
         System.out.println("Symbols:"+l);
         System.out.println("Words:"+words.length);
         System.out.println("Lines:" + k);
+        sc.close();
     }
 }
